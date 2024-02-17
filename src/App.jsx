c31f4dca8a7a47786da5921ae3a69b887a347a72
theo82 <theoaristi53@gmail.com>
2024-02-17 20:56:39 +0200

    route structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import {
   HomeLayout,
   Landing,
@@ -11,8 +12,56 @@ import {
   Checkout,
   Orders,
 } from './pages'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomeLayout />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Landing />,
+      },
+      {
+        path: 'about',
+        element: <About />,
+      },
+      {
+        path: 'products',
+        element: <Products />,
+      },
+      {
+        path: 'products/:id',
+        element: <SingleProduct />,
+      },
+      {
+        path: 'checkout',
+        element: <Checkout />,
+      },
+      {
+        path: 'orders',
+        element: <Orders />,
+      },
+      {
+        path: 'cart',
+        element: <Cart />,
+      },
+    ],
+  },
+  {
+    path: '/login',
+    element: <Login />,
+    errorElement: <Error />,
+  },
+  {
+    path: '/register',
+    element: <Register />,
+    errorElement: <Error />,
+  },
+])
 const App = () => {
-  return <h1 className='text-3xl font-bold m-4underline'>Hello world!</h1>
+  return <RouterProvider router={router} />
 }
 
 export default App
