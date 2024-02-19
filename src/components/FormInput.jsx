052bb6f8526c52437c1e33315cb113d009cca373
theo82 <theoaristi53@gmail.com>
2024-02-19 20:36:15 +0200

    Login Page

--- src/components/FormInput.jsx
+++ src/components/FormInput.jsx
@@ -1,3 +1,12 @@
+/**
+ * Generates a form input element with the provided label, name, type, and default value.
+ *
+ * @param {object} label - The label for the input field
+ * @param {string} name - The name of the input field
+ * @param {string} type - The type of the input field
+ * @param {string} defaultValue - The default value for the input field
+ * @return {JSX.Element} A form input element
+ */
 const FormInput = ({ label, name, type, defaultValue }) => {
   return (
     <div className='form-control'>
---
+++ src/components/SubmitBtn.jsx
@@ -0,0 +1,9 @@
+const SubmitBtn = ({ text }) => {
+  return (
+    <button type='submit' className='btn btn-primary'>
+      {text}
+    </button>
+  )
+}
+
+export default SubmitBtn
