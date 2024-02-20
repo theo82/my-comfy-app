3d9b93f1e2a3474dd46f6368df3758c0e1853060
theo82 <theoaristi53@gmail.com>
2024-02-20 21:11:44 +0200

    Register Page

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,7 +1,26 @@
-import React from 'react'
+import { FormInput, SubmitBtn } from '../components'
+import { Form, Link } from 'react-router-dom'
 
 const Register = () => {
-  return <h1 className='text-4xl'>Register</h1>
+  return (
+    <section className='h-screen grid place-items-center'>
+      <Form
+        method='post'
+        className='card w-96 p-8 bg-base-100 shadow-lg flex flex-col gap-y-4'
+      >
+        <h1 className='text-center text-3xl font-bold'>Register</h1>
+        <FormInput label='username' name='username' type='text' />
+        <FormInput label='email' name='email' type='email' />
+        <FormInput label='password' name='password' type='password' />
+        <div className='mt-4'>
+          <SubmitBtn text='register' />
+        </div>
+        <p className='text-center'>
+          Already a member? <Link to='/login'>Login</Link>
+        </p>
+      </Form>
+    </section>
+  )
 }
 
 export default Register
