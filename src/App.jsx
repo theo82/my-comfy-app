076b9e5f2476ed0d85ed6f0642acd1fd5eab4ab3
theo82 <theoaristi53@gmail.com>
2024-03-01 19:32:07 +0200

    Landing Loader

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,10 @@ import {
   Orders,
 } from './pages'
 
+import { ErrorElement } from './components'
+
+import { loader as landingLoader } from './pages/Landing'
+
 const router = createBrowserRouter([
   {
     path: '/',
@@ -22,6 +26,8 @@ const router = createBrowserRouter([
       {
         index: true,
         element: <Landing />,
+        errorElement: <ErrorElement />,
+        loader: landingLoader,
       },
       {
         path: 'about',
--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,4 +1,13 @@
 import { Hero } from '../components'
+
+const url = '/products?featured=true'
+import { customFetch } from '../utils'
+export const loader = async () => {
+  const response = await customFetch(url)
+  const products = response.data.data
+  return products
+}
+
 const Landing = () => {
   return (
     <>
