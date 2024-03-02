de3dc8f3eb7e8cb96c67afb55904181787e757b3
theo82 <theoaristi53@gmail.com>
2024-03-02 20:46:58 +0200

    Featured Products

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,17 +1,17 @@
-import { Hero } from '../components'
-
-const url = '/products?featured=true'
+import { FeaturedProducts, Hero } from '../components'
+const url = 'products?featured=true'
 import { customFetch } from '../utils'
 export const loader = async () => {
   const response = await customFetch(url)
   const products = response.data.data
-  return products
+  return { products }
 }
 
 const Landing = () => {
   return (
     <>
       <Hero />
+      <FeaturedProducts />
     </>
   )
 }
