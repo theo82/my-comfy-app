3989fc05e3fb5c65757ed4091da9a284843f7073
theo82 <theoaristi53@gmail.com>
2024-03-13 13:00:15 +0200

    Generate Amount Inputs

--- src/pages/SingleProduct.jsx
+++ src/pages/SingleProduct.jsx
@@ -1,5 +1,5 @@
 import { useLoaderData } from 'react-router-dom'
-import { customFetch, formatPrice } from '../utils'
+import { customFetch, formatPrice, generateAmountOptions } from '../utils'
 import { Link } from 'react-router-dom'
 import { useState } from 'react'
 
@@ -13,7 +13,11 @@ const SingleProduct = () => {
     product.attributes
   const dollarsAmount = formatPrice(price)
   const [productColor, setProductColor] = useState(colors[0])
-  console.log(product)
+  const [amount, setAmount] = useState(1)
+
+  const handleAmount = (e) => {
+    setAmount(parseInt(e.target.value))
+  }
   return (
     <section>
       <div className='text-md breadcrumbs'>
@@ -62,6 +66,33 @@ const SingleProduct = () => {
                 )
               })}
             </div>
+          </div>
+          {/* AMOUNT*/}
+          <div className='form-control w-full max-w-xs'>
+            <label className='label' htmlFor='amount'>
+              <h4 className='text-md font-medium tracking-wider capitalize'>
+                amount
+              </h4>
+            </label>
+
+            <select
+              className='select select-secondary select-bordered select-md'
+              name='amount'
+              id='amount'
+              value={amount}
+              onChange={handleAmount}
+            >
+              {generateAmountOptions(20)}
+            </select>
+          </div>
+          {/* CART*/}
+          <div className='mt-10'>
+            <button
+              className='btn btn-secondary btn-md'
+              onClick={() => console.log('add to cart')}
+            >
+              Add to cart
+            </button>
           </div>
         </div>
       </div>
