ad80da9cf12ef2cf12fa29cd5281f4ee67e3b3ab
theo82 <theoaristi53@gmail.com>
2024-03-13 13:33:02 +0200

    Products Page - Setup

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ import { ErrorElement } from './components'
 
 import { loader as landingLoader } from './pages/Landing'
 import { loader as singleProductLoader } from './pages/SingleProduct'
-
+import { loader as productsLoader } from './pages/Products'
 
 const router = createBrowserRouter([
   {
@@ -38,6 +38,8 @@ const router = createBrowserRouter([
       {
         path: 'products',
         element: <Products />,
+        errorElement: <ErrorElement />,
+        loader: productsLoader,
       },
       {
         path: 'products/:id',
